9b8fe2e3cc9b128db2655bb4400ad4005e1e10f0
Augusto2306 <luisaugustomarques@hotmail.com>
2025-03-27 23:18:48 -0300

    level 2 implementation cumulative score

--- code/Const.py
+++ code/Const.py
@@ -17,7 +17,11 @@ ENTITY_DAMAGE = {
     'Level1Bg4': 0,
     'Level1Bg5': 0,
     'Level1Bg6': 0,
-
+    'Level2Bg0': 0,
+    'Level2Bg1': 0,
+    'Level2Bg2': 0,
+    'Level2Bg3': 0,
+    'Level2Bg4': 0,
     'Player1': 1,
     'Player1Shot': 25,
     'Player2': 1,
@@ -35,10 +39,14 @@ ENTITY_HEALTH = {
     'Level1Bg4': 999,
     'Level1Bg5': 999,
     'Level1Bg6': 999,
-
-    'Player1': 300,
+    'Level2Bg0': 999,
+    'Level2Bg1': 999,
+    'Level2Bg2': 999,
+    'Level2Bg3': 999,
+    'Level2Bg4': 999,
+    'Player1': 100,
     'Player1Shot': 1,
-    'Player2': 300,
+    'Player2': 100,
     'Player2Shot': 1,
     'Enemy1': 50,
     'Enemy1Shot': 1,
@@ -54,7 +62,11 @@ ENTITY_SCORE = {
     'Level1Bg4': 0,
     'Level1Bg5': 0,
     'Level1Bg6': 0,
-
+    'Level2Bg0': 0,
+    'Level2Bg1': 0,
+    'Level2Bg2': 0,
+    'Level2Bg3': 0,
+    'Level2Bg4': 0,
     'Player1': 0,
     'Player1Shot': 20,
     'Player2': 0,
@@ -72,6 +84,11 @@ ENTITY_SPEED = {
     'Level1Bg4': 4,
     'Level1Bg5': 5,
     'Level1Bg6': 6,
+    'Level2Bg0': 0,
+    'Level2Bg1': 1,
+    'Level2Bg2': 2,
+    'Level2Bg3': 3,
+    'Level2Bg4': 4,
     'Player1': 3,
     'Player1Shot': 3,
     'Player2': 3,
@@ -89,6 +106,7 @@ ENTITY_SHOT_DELAY = {'Player1': 20,
 
 }
 EVENT_ENEMY = pygame.USEREVENT +1
+EVENT_TIMEOUT = pygame.USEREVENT +2
 
 #  M
 MENU_OPTION = ('NEW GAME 1P',
@@ -111,6 +129,9 @@ PLAYER_KEY_SHOOT = {'Player1': pygame.K_RALT,
 
 #  S
 SPAWN_TIME = 4000
+
+TIMEOUT_STEP = 100  #100ms
+TIMEOUT_LEVEL = 20000  #20s
 
 #  W
 WIN_WIDTH = 576
